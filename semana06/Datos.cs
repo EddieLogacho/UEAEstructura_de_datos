35551b77c2fbdb888468d5a66b9bf51ba58c6b50
Edison <eg.logachot@uea.edu.ec>
2025-07-06 14:45:34 -0500

    s6
    s6

--- semana06/Datos.cs
+++ semana06/Datos.cs
@@ -1,13 +1,24 @@
 using System;
-using System.Collections.Generic;
 
 namespace semana06
 {
+    public class NodoDato
+    {
+        public double Valor;
+        public NodoDato Siguiente;
+
+        public NodoDato(double valor)
+        {
+            Valor = valor;
+            Siguiente = null;
+        }
+    }
+
     public class Datos
     {
-        private List<double> listaPrincipal = new List<double>();
-        private List<double> menoresIgual = new List<double>();
-        private List<double> mayores = new List<double>();
+        private NodoDato cabezaPrincipal;
+        private NodoDato cabezaMenoresIgual;
+        private NodoDato cabezaMayores;
         private double promedio;
 
         public void CargarDatos()
@@ -19,43 +30,115 @@ namespace semana06
             {
                 Console.Write($"Ingrese el dato #{i + 1}: ");
                 double valor = double.Parse(Console.ReadLine());
-                listaPrincipal.Add(valor);
+                AgregarDato(valor);
+            }
+        }
+
+        private void AgregarDato(double valor)
+        {
+            NodoDato nuevo = new NodoDato(valor);
+            if (cabezaPrincipal == null)
+            {
+                cabezaPrincipal = nuevo;
+            }
+            else
+            {
+                NodoDato actual = cabezaPrincipal;
+                while (actual.Siguiente != null)
+                    actual = actual.Siguiente;
+                actual.Siguiente = nuevo;
             }
         }
 
         public void CalcularPromedioYSeparar()
         {
             double suma = 0;
-            foreach (double d in listaPrincipal)
-                suma += d;
+            int contador = 0;
 
-            promedio = suma / listaPrincipal.Count;
-
-            foreach (double d in listaPrincipal)
+            NodoDato actual = cabezaPrincipal;
+            while (actual != null)
             {
-                if (d <= promedio)
-                    menoresIgual.Add(d);
+                suma += actual.Valor;
+                contador++;
+                actual = actual.Siguiente;
+            }
+
+            if (contador == 0)
+            {
+                Console.WriteLine("No hay datos para calcular el promedio.");
+                return;
+            }
+
+            promedio = suma / contador;
+
+            actual = cabezaPrincipal;
+            while (actual != null)
+            {
+                if (actual.Valor <= promedio)
+                    AgregarMenorIgual(actual.Valor);
                 else
-                    mayores.Add(d);
+                    AgregarMayor(actual.Valor);
+                actual = actual.Siguiente;
+            }
+        }
+
+        private void AgregarMenorIgual(double valor)
+        {
+            NodoDato nuevo = new NodoDato(valor);
+            if (cabezaMenoresIgual == null)
+                cabezaMenoresIgual = nuevo;
+            else
+            {
+                NodoDato actual = cabezaMenoresIgual;
+                while (actual.Siguiente != null)
+                    actual = actual.Siguiente;
+                actual.Siguiente = nuevo;
+            }
+        }
+
+        private void AgregarMayor(double valor)
+        {
+            NodoDato nuevo = new NodoDato(valor);
+            if (cabezaMayores == null)
+                cabezaMayores = nuevo;
+            else
+            {
+                NodoDato actual = cabezaMayores;
+                while (actual.Siguiente != null)
+                    actual = actual.Siguiente;
+                actual.Siguiente = nuevo;
             }
         }
 
         public void MostrarResultados()
         {
-            Console.WriteLine("\n Lista de datos ingresados:");
-            foreach (var d in listaPrincipal)
-                Console.Write(d + " ");
+            Console.WriteLine("\n Lista principal:");
+            MostrarLista(cabezaPrincipal);
 
             Console.WriteLine($"\n Promedio: {promedio:F2}");
 
-            Console.WriteLine(" Datos menores o iguales al promedio:");
-            foreach (var d in menoresIgual)
-                Console.Write(d + " ");
+            Console.WriteLine("Datos menores o iguales al promedio:");
+            MostrarLista(cabezaMenoresIgual);
 
-            Console.WriteLine("\nDatos mayores al promedio:");
-            foreach (var d in mayores)
-                Console.Write(d + " ");
+            Console.WriteLine(" Datos mayores al promedio:");
+            MostrarLista(cabezaMayores);
+        }
+
+        private void MostrarLista(NodoDato cabeza)
+        {
+            if (cabeza == null)
+            {
+                Console.WriteLine("(vacía)");
+                return;
+            }
+
+            NodoDato actual = cabeza;
+            while (actual != null)
+            {
+                Console.Write(actual.Valor + " ");
+                actual = actual.Siguiente;
+            }
+            Console.WriteLine();
         }
     }
 }
-
